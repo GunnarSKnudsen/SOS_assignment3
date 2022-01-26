4d8564066aa8c2a9bb162bd5262d2c24434b1808
MagnusWagner <magnuswagner@hotmail.de>
2022-01-26 11:25:08 +0100

    experiments Magnus

---
+++ java_folder/experiment_03/90x90/createDistanceMatrix.bat
@@ -0,0 +1,1 @@
+C:\\Users\\Magnus\\Documents\\GitHub\\SOS_assignment3\\java_folder\\somtoolbox_src\\somtoolbox.bat DistanceMatrixWriter -v .\\input.vec -c .\\class_info.cls  .\\input_distances.dist
---
+++ java_folder/experiment_04/30x30_S30/visualizer.bat
@@ -0,0 +1,9 @@
+C:\\Users\\Magnus\\Documents\\GitHub\\SOS_assignment3\\java_folder\\somtoolbox_src\\somtoolbox.bat VisualisationImageSaver ^
+-u .\\exp03_30.unit.gz ^
+-w .\\exp03_30.wgt ^
+-v .\\input.vec ^
+-t .\\template.tv ^
+--dw .\\exp03_90.dwm.gz ^
+-c .\\class_info.cls ^
+-o vis03_30 ^
+PMatrix
